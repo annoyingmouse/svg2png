8373cf639c593a9df6e90ce5b1d5e9510b3ec39f
Dominic Myers <annoyingmouse@gmail.com>
2017-12-12 13:55:55 +0000

    Think we're nearly there.

--- js/main.es6
+++ js/main.es6
@@ -1,4 +1,7 @@
-//svg4everybody();
+/*
+ * https://community.spiceworks.com/topic/411506-detecting-ie11
+ * Not pretty, but it works
+ */
 const getInternetExplorerVersion = () => {
     let rv = -1;
     const ua = navigator.userAgent;
@@ -13,58 +16,65 @@ const getInternetExplorerVersion = () => {
     }
     return rv;
 };
+// If we're looking at IE11
 if(getInternetExplorerVersion() === 11){
     // Get all the SVG tags
     const svgs = document.getElementsByTagName("svg");
     for(let i = 0; i < svgs.length; i++){
+        // Get a reference to the initial SVG element
+        const svgElement = svgs[i];
         // Check to see if the SVG tag has a USE tag (indicating an external asset).
         const external = svgs[i].getElementsByTagName("use");
+        // Get dimensions of the SVG
+        const height = parseInt(svgs[i].getAttribute("height"), 10);
+        const width = parseInt(svgs[i].getAttribute("width"), 10);
         if(external.length){ // Truthy/Falsy
+            // Get the link to the external SVG from the first USE tag (we only have the one, right?)
             const href = external[0].getAttribute("xlink:href");
+            // Split it on the hash symbol
             const hrefParts = href.split("#");
+            // Do the ajax shimmy
             let xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
+                // If we're sorted
                 if (this.readyState === 4 && this.status === 200) {
+                    // Get the XML string
                     const xmlString = this.responseText;
+                    // Create a parse
                     const parser = new DOMParser();
+                    // Parse the XML string as XML
                     const svgDoc = parser.parseFromString(xmlString, "text/xml");
-                    // console.log(svgDoc);
+                    // Get the specific SVG from the parsed SVG
                     const glyph = svgDoc.getElementById(hrefParts[1]);
-                    // console.log(glyph);
+                    // Create an SVG serializer
                     const XMLS = new XMLSerializer();
+                    // Convert the SVG XML into a string
                     const svgStr = XMLS.serializeToString(glyph);
-                    // console.log(svgStr);
-                    const svgBlob = new Blob([svgStr]);
-                    // console.log(svgBlob);
-                    const domURL = self.URL; // || self.webkitURL || self;
-                    const url = domURL.createObjectURL(svgBlob);
-                    // console.log(url);
+                    // Create a Blob from the string
+                    const svgBlob = new Blob([svgStr], {
+                        "type": 'image/svg+xml;charset=utf-8'
+                    });
+                    // Create an URL from the Blob
+                    const url = URL.createObjectURL(svgBlob);
+                    // Create an IMG tag
                     const img = new Image;
+                    // Once the image is loaded...
                     img.onload = function(){
-
-                    }
-
-
+                        // ...replace the original element with the IMG...
+                        svgElement.parentNode.replaceChild(img, svgElement);
+                        // ... and tidy up after ourselves.
+                        URL.revokeObjectURL(url);
+                    };
+                    // Set the IMG tag to have the src of the URL
+                    img.src = url;
+                    // Set its height and width
+                    img.height = height;
+                    img.width = width;
                 }
             };
+            // Grab the SVG element
             xhttp.open("GET", hrefParts[0], true);
             xhttp.send();
-
         }
     }
-
-
 }
-
-const XMLtoString = (elem) => {
-    let serialized = null;
-    try {
-        // XMLSerializer exists in current Mozilla browsers
-        const serializer = new XMLSerializer();
-        serialized = serializer.serializeToString(elem);
-    }catch (e) {
-        // Internet Explorer has a different approach to serializing XML
-        serialized = elem.xml;
-    }
-    return serialized;
-};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
-//svg4everybody();
+/*
+ * https://community.spiceworks.com/topic/411506-detecting-ie11
+ * Not pretty, but it works
+ */
 var getInternetExplorerVersion = function getInternetExplorerVersion() {
     var rv = -1;
     var ua = navigator.userAgent;
@@ -15,54 +18,71 @@ var getInternetExplorerVersion = function getInternetExplorerVersion() {
     }
     return rv;
 };
+// If we're looking at IE11
 if (getInternetExplorerVersion() === 11) {
     // Get all the SVG tags
     var svgs = document.getElementsByTagName("svg");
-    for (var i = 0; i < svgs.length; i++) {
+
+    var _loop = function _loop(i) {
+        // Get a reference to the initial SVG element
+        var svgElement = svgs[i];
         // Check to see if the SVG tag has a USE tag (indicating an external asset).
         var external = svgs[i].getElementsByTagName("use");
+        // Get dimensions of the SVG
+        var height = parseInt(svgs[i].getAttribute("height"), 10);
+        var width = parseInt(svgs[i].getAttribute("width"), 10);
         if (external.length) {
-            (function () {
-                // Truthy/Falsy
-                var href = external[0].getAttribute("xlink:href");
-                var hrefParts = href.split("#");
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function () {
-                    if (this.readyState === 4 && this.status === 200) {
-                        var xmlString = this.responseText;
-                        var parser = new DOMParser();
-                        var svgDoc = parser.parseFromString(xmlString, "text/xml");
-                        // console.log(svgDoc);
-                        var glyph = svgDoc.getElementById(hrefParts[1]);
-                        // console.log(glyph);
-                        var XMLS = new XMLSerializer();
-                        var svgStr = XMLS.serializeToString(glyph);
-                        // console.log(svgStr);
-                        var svgBlob = new Blob([svgStr]);
-                        // console.log(svgBlob);
-                        var domURL = self.URL; // || self.webkitURL || self;
-                        var url = domURL.createObjectURL(svgBlob);
-                        // console.log(url);
-                        var img = new Image();
-                        img.onload = function () {};
-                    }
-                };
-                xhttp.open("GET", hrefParts[0], true);
-                xhttp.send();
-            })();
+            // Truthy/Falsy
+            // Get the link to the external SVG from the first USE tag (we only have the one, right?)
+            var href = external[0].getAttribute("xlink:href");
+            // Split it on the hash symbol
+            var hrefParts = href.split("#");
+            // Do the ajax shimmy
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                // If we're sorted
+                if (this.readyState === 4 && this.status === 200) {
+                    // Get the XML string
+                    var xmlString = this.responseText;
+                    // Create a parse
+                    var parser = new DOMParser();
+                    // Parse the XML string as XML
+                    var svgDoc = parser.parseFromString(xmlString, "text/xml");
+                    // Get the specific SVG from the parsed SVG
+                    var glyph = svgDoc.getElementById(hrefParts[1]);
+                    // Create an SVG serializer
+                    var XMLS = new XMLSerializer();
+                    // Convert the SVG XML into a string
+                    var svgStr = XMLS.serializeToString(glyph);
+                    // Create a Blob from the string
+                    var svgBlob = new Blob([svgStr], {
+                        "type": 'image/svg+xml;charset=utf-8'
+                    });
+                    // Create an URL from the Blob
+                    var url = URL.createObjectURL(svgBlob);
+                    // Create an IMG tag
+                    var img = new Image();
+                    // Once the image is loaded...
+                    img.onload = function () {
+                        // ...replace the original element with the IMG...
+                        svgElement.parentNode.replaceChild(img, svgElement);
+                        // ... and tidy up after ourselves.
+                        URL.revokeObjectURL(url);
+                    };
+                    // Set the IMG tag to have the src of the URL
+                    img.src = url;
+                    // Set its height and width
+                    img.height = height;
+                    img.width = width;
+                }
+            };
+            // Grab the SVG element
+            xhttp.open("GET", hrefParts[0], true);
+            xhttp.send();
         }
+    };
+
+    for (var i = 0; i < svgs.length; i++) {
+        _loop(i);
     }
 }
-
-var XMLtoString = function XMLtoString(elem) {
-    var serialized = null;
-    try {
-        // XMLSerializer exists in current Mozilla browsers
-        var serializer = new XMLSerializer();
-        serialized = serializer.serializeToString(elem);
-    } catch (e) {
-        // Internet Explorer has a different approach to serializing XML
-        serialized = elem.xml;
-    }
-    return serialized;
-};
